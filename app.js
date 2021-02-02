27aa3322572dc2a7ba15fa4d228a90ac04b14e3e
jcrotzerrn <55601326+jcrotzerrn@users.noreply.github.com>
2021-02-02 10:38:40 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,68 @@
+//create a class
+class Die {
+    constructor(div, value) {
+        this.value = value;
+        this.div = div;
+    }
+    roll() {
+        this.div = document.createElement('div');
+        this.value = this.randNum();
+        this.div.innerText = this.value;
+        this.div.id = "square";
+        this.div.classList.add(
+            "shadow",
+            "text-secondary",
+            "d-flex",
+            "align-items-center",
+            "die",
+            "justify-content-center"
+        );
+        diceContainer.appendChild(this.div);
+
+        //add event listener for single click
+        this.div.addEventListener('click', () => {
+            this.reroll();
+        });
+        //add event listener for double click
+        this.div.addEventListener('dblclick', () => {
+            allDie.splice(allDie.indexOf(this), 1);
+            this.div.remove();
+        });
+    }
+
+//creating a random number between 1 and 6
+randNum() {
+    return Math.floor(Math.random() * 6) + 1;
+}
+//method for updating die value when button click
+reroll() {
+    this.value = this.randNum();
+    this.div.innerText = this.value;
+    }
+}
+
+let allDie = [];
+
+const diceContainer = document.getElementById('dice-container');
+const addDieBtn = document.getElementById('add-new-die');
+const rerollBtn = document.getElementById('re-roll');
+const sumBtn = document.getElementById('sum-dice');
+
+addDieBtn.addEventListener('click', function() {
+    const die = new Die();
+    const value = die.roll();
+    allDie.push(die);
+});
+
+rerollBtn.addEventListener('click', function() {
+    for (let i = 0; i < allDie.length; i++) {
+        allDie[i].reroll();
+    }
+});
+sumBtn.addEventListener('click', function() {
+    let sumDieValue = 0;
+    for (let i = 0; i < allDie.length; i++) {
+        sumDieValue += allDie[i].value;
+    }
+    alert(`Dice sum is: ${sumDieValue}.`);
+});
